494c3755692ef1f9b2a24d9910fe8e0ef191326b
ziadsobri <39057465+ziadsobri@users.noreply.github.com>
2019-07-06 12:18:31 +0700

    Update index.php

--- index.php
+++ index.php
@@ -26,6 +26,10 @@
        <input type="submit" name="load_data" value="Load Data" />
  </form>
  <?php
+    // SQL Server Extension Sample Code:
+$connectionInfo = array("UID" => "bobby@bobbydeveloper", "pwd" => "t130b315", "Database" => "registrasi", "LoginTimeout" => 30, "Encrypt" => 1, "TrustServerCertificate" => 0);
+$serverName = "tcp:bobbydeveloper.database.windows.net,1433";
+$conn = sqlsrv_connect($serverName, $connectionInfo);
     try {
         $conn = new PDO("sqlsrv:server = tcp:bobbydeveloper.database.windows.net,1433; Database = registrasi", "bobby", "t130b315");
         $conn->setAttribute( PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION );
