6da245e49196fd38986475370bd361638ac7a693
ziadsobri <39057465+ziadsobri@users.noreply.github.com>
2019-07-11 22:50:59 +0700

    Update create-table.sql

--- create-table.sql
+++ create-table.sql
@@ -1,7 +1,7 @@
-create table [dbo].[registrasi](
+create table [dbo].[Registration](
     id INT NOT NULL IDENTITY(1,1) PRIMARY KEY(id),
     name VARCHAR(30),
     email VARCHAR(30),
     job VARCHAR(30),
-    date date
+    date DATE
 );
